61c80504db8dc3346ea7da2d42aa3f94bcc33b24
Chris <cl114@illinois.edu>
2021-12-29 19:42:43 -0600

    new pages and styling

--- musices-app/src/App.js
+++ musices-app/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import About from "./pages/About"
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Player from "./pages/Player";
 
 function App() {
 	return (
@@ -10,8 +11,10 @@ function App() {
       		<Navbar />
 
       		<Switch>
-        		<Route path='/' exact component = {Home} />
-        		<Route path='/about' exact component = {About} />
+        		<Route path = '/' exact component = {Home} />
+        		<Route path = '/about' exact component = {About} />
+				<Route path = '/player' exact component = {Player} />
+
       		</Switch>
 
     	</Router>
--- musices-app/src/components/Navbar.js
+++ musices-app/src/components/Navbar.js
@@ -5,18 +5,25 @@ import {  Link } from "react-router-dom";
 const Navbar= () =>{
   	return (
 		<header class = "header">
+
+			<div className = 'logo-home'>
+            	<img className = "logo-home-image" src = "images/musices-logo-removebg-preview.png" alt =""/>  
+            </div> 
     
 			<div class = "mid">
 				<ul class = "navbar">
 					<li>
-						<Link to = "/"> Home </Link>
+						<b> <Link to = "/"> Home </Link> </b>
 					</li>
 					
 					<li>
-						<Link to = "/about"> About </Link>
+						<b> <Link to = "/about"> About </Link> </b>
 					</li>
 
-					
+					<li> 
+						<b> <Link to = "/player"> Player </Link> </b>
+					</li>
+
 				</ul>
 			</div>
 
