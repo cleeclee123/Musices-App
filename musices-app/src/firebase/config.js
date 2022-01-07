615b3fa4c72bdbf1494d78b217a70aa2cdbdde03
Chris <cl114@illinois.edu>
2022-01-07 17:46:03 -0600

    format

--- musices-app/src/firebase/config.js
+++ musices-app/src/firebase/config.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDzHh-HS6OLIvb-_XO5ptQ7dKKgHdZgGY",
-  authDomain: "musices-app.firebaseapp.com",
-  projectId: "musices-app",
-  storageBucket: "musices-app.appspot.com",
-  messagingSenderId: "984749932392",
-  appId: "1:984749932392:web:c50295d0d36e19a2b6a0dd",
-  measurementId: "G-7VCY6270PT"
+	apiKey: "AIzaSyCDzHh-HS6OLIvb-_XO5ptQ7dKKgHdZgGY",
+	authDomain: "musices-app.firebaseapp.com",
+	projectId: "musices-app",
+	storageBucket: "musices-app.appspot.com",
+	messagingSenderId: "984749932392",
+	appId: "1:984749932392:web:c50295d0d36e19a2b6a0dd",
+	measurementId: "G-7VCY6270PT"
 };
 
 const app = initializeApp(firebaseConfig);
